4b3a80792ad7be46e5db9add520d138a1718a62e
unknown <dandarad196@gmail.com>
2025-02-03 05:16:47 -0300

    Task 07: Criação de toda parte lógica da função cadastrar.

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,4 +1,6 @@
 import './Login.css';
+import '../cadastro/Cadastro';
+import { Link } from 'react-router-dom';
 
 function Login() {
 
@@ -39,9 +41,12 @@ function Login() {
                     <hr className="border-slate-800 w-full" />
 
                     <p>
-                        Ainda não tem uma conta?{' '}
-                        Cadastre-se
-                    </p>
+	Ainda não tem uma conta?{' '}
+    <Link to="/cadastro" className="text-indigo-800 hover:underline">
+    	Cadastre-se
+	</Link>
+</p>
+
                 </form>
                 <div className="fundoLogin hidden lg:block"></div>
             </div>
