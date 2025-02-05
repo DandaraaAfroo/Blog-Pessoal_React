cfe4203e9a4434618b2145ee54583110b8bc8441
unknown <dandarad196@gmail.com>
2025-02-05 18:56:06 -0300

    Task 14: Criação do componente Deletar Tema - Estilização.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import Cadastro from './pages/cadastro/Cadastro'
 import FormTema from './components/temas/formtema/FormTema'
+import DeletarTema from './components/temas/deletartema/Deletartema'
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
               <Route path="/temas" element={<ListaTemas />} />
               <Route path="/cadastrartema" element={<FormTema />} />
               <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
